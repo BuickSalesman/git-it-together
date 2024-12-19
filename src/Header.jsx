83244ed6bd1474551d21a677b979d621ce05c007
BuickSalesman <adam.odonnell.e@gmail.com>
2024-12-19 17:48:19 -0500

    added links to signup and login

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom";
+
 export function Header() {
   return (
     <header>
-      <nav></nav>
+      <nav>
+        <Link to="/">Home</Link>
+        {localStorage.jwt === undefined ? (
+          <>
+            <Link to="/signup">Signup</Link>
+            <Link to="/login">Login</Link>
+          </>
+        ) : (
+          <></>
+        )}
+      </nav>
     </header>
   );
 }
