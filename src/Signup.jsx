b690b929725f16990f0ff2f18afe351f7e2c090d
BuickSalesman <adam.odonnell.e@gmail.com>
2025-01-04 16:44:17 -0500

    finished signup component

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -3,13 +3,43 @@ import { useState } from "react";
 
 export function Signup() {
   const [errors, setErrors] = useState([]);
-  const [name, setName] = useState("");
-  const [status, setStatus] = useState(null);
 
   const handleSubmit = (event) => {
     event.preventDefault();
     setErrors([]);
     const params = new FormData(event.target);
-    axios.post("/users.json");
+    axios
+      .post("http://localhost:8000/users/", params)
+      .then((response) => {
+        console.log(response.data);
+        event.target.reset();
+        window.location.href = "/";
+      })
+      .catch((error) => {
+        console.log(error.response.data.errors);
+        setErrors(error.response.data.errors);
+      });
   };
+
+  return (
+    <div id="signup">
+      <h1>Signup</h1>
+      <ul>
+        {errors.map((error) => (
+          <li key={error} {...error}></li>
+        ))}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <div>
+          Username: <input name="name" type="text" />
+        </div>
+        <div>
+          Password: <input name="password" type="password" />
+        </div>
+        <div>
+          Password Confirmation: <input name="password_confirmation" type="text" />
+        </div>
+      </form>
+    </div>
+  );
 }
