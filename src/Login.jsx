2f64e10581a5362335c27c164f7732699506bb91
BuickSalesman <adam.odonnell.e@gmail.com>
2025-01-05 18:01:39 -0500

    Finished refactoring Login handlesubmit

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,17 +1,32 @@
 import axios from "axios";
 import { useState } from "react";
 
-const jwt = localStorage.getItem("jwt");
-if (jwt) {
-  axios.defaults.headers.common["Authorization"] = `Bearer ${jwt}`;
+const accessToken = localStorage.getItem("accessToken");
+if (accessToken) {
+  axios.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
 }
 
 export function Login() {
   const [errors, setErrors] = useState([]);
 
   const handleSubmit = (event) => {
-    event.preventDefaults();
+    event.preventDefault();
     setErrors([]);
+
+    const params = new FormData(event.target);
+    axios
+      .post("http://localhost:8000/auth/token/", params)
+      .then((response) => {
+        const accessToken = response.data.access;
+        axios.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
+        localStorage.setItem("accessToken", accessToken);
+        event.target.reset();
+        window.location.href = "/";
+      })
+      .catch((error) => {
+        console.log(error.response);
+        setErrors(["Invalid email or password"]);
+      });
   };
 
   return (
