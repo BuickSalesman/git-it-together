56dd1ecd039aba6df59dd2686a1c68d44445d965
BuickSalesman <adam.odonnell.e@gmail.com>
2025-01-17 21:33:42 -0500

    updated repo card

--- src/components/RepoCard.jsx
+++ src/components/RepoCard.jsx
@@ -3,9 +3,7 @@ import "./RepoCard.css";
 export function RepoCard() {
   return (
     <div className="repo-card">
-      <h1>title</h1>
-      <h2>calednar</h2>
-      <h3>streak: 18 days</h3>
+      <Calendar />
     </div>
   );
 }
