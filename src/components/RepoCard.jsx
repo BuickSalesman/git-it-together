139b422132554684332c6d2b6f565a509487a51a
BuickSalesman <adam.odonnell.e@gmail.com>
2025-01-19 20:10:12 -0500

    started playing with React Calndar

--- src/components/RepoCard.jsx
+++ src/components/RepoCard.jsx
@@ -2,18 +2,27 @@ import "./RepoCard.css";
 import ReactCalendarHeatmap from "react-calendar-heatmap";
 import "react-calendar-heatmap/dist/styles.css";
 
+const today = new Date();
+
 export function RepoCard() {
   return (
     <div className="repo-card">
       <ReactCalendarHeatmap
-        startDate={new Date("2016-01-01")}
-        endDate={new Date("2016-04-01")}
+        startDate={shiftDate(today, -150)}
+        endDate={today}
         values={[
-          { date: "2016-01-01", count: 12 },
-          { date: "2016-01-22", count: 122 },
-          { date: "2016-01-30", count: 38 },
+          { date: "2025-01-01", count: 12 },
+          { date: "2025-01-02", count: 122 },
+          { date: "2025-01-10", count: 38 },
         ]}
+        showWeekdayLabels={true}
       />
     </div>
   );
+
+  function shiftDate(date, numDays) {
+    const newDate = new Date(date);
+    newDate.setDate(newDate.getDate() + numDays);
+    return newDate;
+  }
 }
