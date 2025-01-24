ed870d8dc1bf22c9af4a43ee7f8c9966f381038a
BuickSalesman <adam.odonnell.e@gmail.com>
2025-01-24 14:00:44 -0500

    added total commits to repo card

--- src/components/RepoCard.jsx
+++ src/components/RepoCard.jsx
@@ -2,13 +2,15 @@ import "./RepoCard.css";
 import Heatmap from "./HeatmapCalendar";
 
 export function RepoCard({ repo, commits }) {
+  const totalCommits = commits.length;
+
   return (
     <div className="repo-card">
       <h3>{repo.name}</h3>
       <div className="calendar-container">
         <Heatmap commits={commits} />
       </div>
-      <div className="total-commits-container">total commits: 35</div>
+      <div className="total-commits-container">total commits: {totalCommits}</div>
       <div className="streak-container">longest streak: 2 days</div>
       <div className="last-updated">Last commit: {new Date(repo.updated_at).toLocaleString()}</div>
     </div>
