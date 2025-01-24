d9381e54e6629f7ae67d4b3595ae6dfd6c500479
BuickSalesman <adam.odonnell.e@gmail.com>
2025-01-23 23:49:12 -0500

    reworked repo card to display by last updated repo

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,14 +1,41 @@
+import { useState, useEffect } from "react";
 import { RepoCard } from "./RepoCard";
 import { NewRepoContainer } from "./NewRepoContainer";
 import "./Content.css";
+import axios from "axios";
 
 export function Content() {
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+    const fetchRepos = async () => {
+      try {
+        const accessToken = localStorage.getItem("accessToken");
+        const response = await axios.get("http://localhost:8000/repos/", {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+
+        let data = response.data.user_repos;
+
+        data.sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at));
+
+        setRepos(data);
+      } catch (error) {
+        console.log("Error fetching repos:", error);
+      }
+    };
+
+    fetchRepos();
+  }, []);
+
   return (
     <div className="content">
       <div className="repo-container">
-        <RepoCard />
-        <RepoCard />
-        <RepoCard />
+        {repos.map((repo) => (
+          <RepoCard key={repo.id} repo={repo} />
+        ))}
         <NewRepoContainer />
       </div>
     </div>
--- src/components/RepoCard.jsx
+++ src/components/RepoCard.jsx
@@ -1,14 +1,16 @@
 import "./RepoCard.css";
 import Heatmap from "./HeatmapCalendar";
 
-export function RepoCard() {
+export function RepoCard({ repo }) {
   return (
     <div className="repo-card">
+      <h3>{repo.name}</h3>
       <div className="calendar-container">
         <Heatmap />
       </div>
-      <div className="total-commits-container">total commits: 35</div>
+      <div className="total-commits-container">total x`commits: 35</div>
       <div className="streak-container">longest streak: 2 days</div>
+      <div className="last-updated">Last updated: {new Date(repo.updated_at).toLocaleString()}</div>
     </div>
   );
 }
