6f6665e8576e4f92b77d9caaea31d8b197e8510c
BuickSalesman <adam.odonnell.e@gmail.com>
2025-02-07 14:14:44 -0500

    finally got shading for commits

--- src/components/HeatmapCalendar.jsx
+++ src/components/HeatmapCalendar.jsx
@@ -1,6 +1,7 @@
 import CalHeatmap from "cal-heatmap";
 import "cal-heatmap/cal-heatmap.css";
 import { useEffect, useRef } from "react";
+import "./HeatmapCalendar.css";
 
 const Heatmap = ({ commits, repoCreationDate }) => {
   const heatmapRef = useRef(null);
@@ -10,20 +11,42 @@ const Heatmap = ({ commits, repoCreationDate }) => {
     const cal = new CalHeatmap();
     cal.paint({
       itemSelector: heatmapRef.current,
-      range: 12,
+      range: 13,
       domain: {
         type: "month",
-        label: { text: "MMM" },
+        label: { text: "MMM", position: "top", textAlign: "middle" },
       },
 
-      subDomain: { type: "ghDay", label: "YYYY-MM-DD", radius: 5, width: 50, height: 40 },
+      //change type: "ghDay" for no gaps between months
+      subDomain: { type: "day", label: "YYYY-MM-DD", radius: 5, width: 50, height: 40 },
       date: {
         start: "2024-02-07",
         max: new Date(),
         min: "2024-02-07",
       },
+      data: {
+        source: commits,
+        type: "json",
+        x: "created_at",
+        y: () => 1,
+        aggregator: "sum",
+      },
+      scale: {
+        color: {
+          range: ["yellow", "red", "green"],
+          interpolate: "hsl",
+          type: "linear",
+          domain: [0, 15, 30],
+        },
+      },
     });
-  }, []);
+
+    console.log(commits);
+  }, [commits]);
+
+  if (!commits || commits.length === 0) {
+    return null;
+  }
 
   return <div ref={heatmapRef}></div>;
 };
