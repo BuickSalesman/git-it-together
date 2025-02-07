10888f85e8f8ca964d532b5a9131b349f59f0319
BuickSalesman <adam.odonnell.e@gmail.com>
2025-02-07 15:47:38 -0500

    commiting so I can start testing

--- src/components/HeatmapCalendar.jsx
+++ src/components/HeatmapCalendar.jsx
@@ -8,8 +8,22 @@ const Heatmap = ({ commits, repoCreationDate }) => {
 
   useEffect(() => {
     if (!heatmapRef.current) return;
+
+    const dailyCommits = commits.reduce((acc, c) => {
+      const day = c.created_at.slice(0, 10);
+      acc[day] = (acc[day] || 0) + 1;
+      return acc;
+    }, {});
+
+    const maxDailyCommits = Math.max(...Object.values(dailyCommits));
+
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
     const cal = new CalHeatmap();
     cal.paint({
+      // integrate dark light mode here later
+      // theme: "light",
       itemSelector: heatmapRef.current,
       range: 13,
       domain: {
@@ -18,10 +32,22 @@ const Heatmap = ({ commits, repoCreationDate }) => {
       },
 
       //change type: "ghDay" for no gaps between months
-      subDomain: { type: "day", label: "YYYY-MM-DD", radius: 5, width: 50, height: 40 },
+      subDomain: {
+        type: "day",
+        label: "YYYY-MM-DD",
+        radius: 5,
+        width: 50,
+        height: 40,
+        showOutOfDomain: true,
+        exclude: (date) => {
+          const isExcluded = date.getTime() > today.getTime();
+          console.log("Checking", date, "excluded?", isExcluded);
+          return isExcluded;
+        },
+      },
       date: {
         start: "2024-02-07",
-        max: new Date(),
+        max: today,
         min: "2024-02-07",
       },
       data: {
@@ -33,15 +59,12 @@ const Heatmap = ({ commits, repoCreationDate }) => {
       },
       scale: {
         color: {
-          range: ["yellow", "red", "green"],
-          interpolate: "hsl",
-          type: "linear",
-          domain: [0, 15, 30],
+          range: ["#0D4429", "#016D32", "#26A641", "#3AD353"],
+          type: "quantize",
+          domain: [0, maxDailyCommits],
         },
       },
     });
-
-    console.log(commits);
   }, [commits]);
 
   if (!commits || commits.length === 0) {
