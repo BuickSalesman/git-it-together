0fba7366036f5bee3d56f4ef7968a58588fb19f4
BuickSalesman <adam.odonnell.e@gmail.com>
2025-02-20 22:16:47 -0500

    debugged passing commits to NotesModal

--- src/components/CommitNotesModal.jsx
+++ src/components/CommitNotesModal.jsx
@@ -1,8 +1,15 @@
 export default function NotesModal({ onClose, commits }) {
   return (
     <div>
-      <div>hello world</div>
-      <div>{commits}</div>
+      <div>
+        {commits.map((commit) => (
+          <div key={commit.id}>
+            <h4>{commit.note_title}</h4>
+            <p>{commit.note_body}</p>
+            <p>Created at: {commit.created_at}</p>
+          </div>
+        ))}
+      </div>
       <button onClick={onClose}>&times;</button>
     </div>
   );
